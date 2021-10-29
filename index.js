65643aff1fd49050f42f3fc6a8d867524cc0b292
Gavin Loo <looweiren@gmail.com>
2021-10-30 00:08:45 +0800

    added EJS and basic routing to project

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = 3000;
 
+// Set html templating engine to EJS
+app.set('view engine', 'ejs');
+// Set views directory to get html templating
+app.set('views', path.join(__dirname, "views"));
+
+// Directory to serve all files
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get('/', function (req, res) {
-    res.send("hello world");
+    res.render('home.ejs');
 })
 
 app.get('/pokemon', function (req, res) {
