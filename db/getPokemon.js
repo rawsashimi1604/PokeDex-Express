7d5a50e83eea1c11610c3272e37cf93ebec90f9c
Gavin Loo <looweiren@gmail.com>
2021-12-22 23:25:33 +0800

    added routing to home.ejs, added utility getPokemon.js in db folder.

---
+++ db/getPokemon.js
@@ -0,0 +1,23 @@
+const Pokemon = require('../models/Pokemon');
+
+async function retrieveDataFromDB() {
+    const data = [];
+    for (let i = 1; i <= 151; i++) {
+        const myPokeObj = await Pokemon.findOne({
+            pokeID: i
+        });
+        data.push(myPokeObj);
+    }
+    return data;
+}
+
+async function retrievePokemonFromDB(pokeID) {
+    const myPokeObj = await Pokemon.findOne({
+        pokeID: pokeID
+    });
+    console.log(myPokeObj);
+    return myPokeObj;
+}
+
+exports.retrieveDataFromDB = retrieveDataFromDB;
+exports.retrievePokemonFromDB = retrievePokemonFromDB;
--- routes/allmain.js
+++ routes/allmain.js
@@ -1,31 +1,24 @@
 const express = require('express');
 const router = new express.Router();
-const axios = require('axios');
 const Pokemon = require('../models/Pokemon');
-
-
-async function retrieveDataFromDB() {
-    const data = [];
-    for (let i = 1; i <= 151; i++) {
-        const myPokeObj = await Pokemon.findOne({
-            pokeID: i
-        });
-        data.push(myPokeObj);
-    }
-    return data;
-}
+const getPokemon = require('../db/getPokemon');
 
 router.get('/', async function (req, res) {
-    const data = await retrieveDataFromDB();
+    const data = await getPokemon.retrieveDataFromDB();
     res.render("../views/home.ejs", {
         data: data
     });
-    // res.json(data);
 })
 
 router.get('/pokemon', function (req, res) {
-    dbConnect()
     res.send("I love pokemon!");
+})
+
+router.get('/pokemon/:id', async function (req, res) {
+    const data = await getPokemon.retrievePokemonFromDB(req.params.id);
+    res.render("../views/pokemonPage.ejs", {
+        data: data
+    });
 })
 
 module.exports = router;
